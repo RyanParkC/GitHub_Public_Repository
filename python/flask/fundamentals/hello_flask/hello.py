b50a98b68e90e0f141b6593d8322462498124f96
RyanParkC <hunjinee@gmail.com>
2021-08-09 01:11:24 -0700

    Updating Assignment Files

--- python/flask/fundamentals/hello_flask/hello.py
+++ python/flask/fundamentals/hello_flask/hello.py
@@ -3,7 +3,6 @@
 from flask import Flask, render_template  # Import Flask to allow us to create our app
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 
-
 @app.route('/')
 def index():
     return render_template("index.html", phrase="hello", times=5)	# notice the 2 new named arguments!
@@ -11,6 +10,17 @@ def index():
 @app.route('/success')
 def success():
     return "Success" #http://localhost:5000/success
+
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    student_info = [
+        {'name' : 'Michael', 'age' : 35},
+        {'name' : 'John', 'age' : 30 },
+        {'name' : 'Mark', 'age' : 25},
+        {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], students = student_info)
 
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
