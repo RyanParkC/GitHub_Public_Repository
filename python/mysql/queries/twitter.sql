08afb4a0fd5dd1ffef0ea9641b0119f212295843
Ryan Park <hunjinee@gmail.com>
2021-08-13 18:08:26 -0700

    Adding Assignment Files

--- python/mysql/queries/twitter.sql
+++ python/mysql/queries/twitter.sql
@@ -211,7 +211,32 @@ GROUP BY sites.id;
    -- Reverse Engineering -- Diagram generated (.mwb)
    -- Forward Engineering -- Script generated (.sql)
 
-SELECT clients.company, SUM(billing.amount) AS total_sales 
-FROM clients 
-LEFT JOIN billing ON billing.client_id = client.id 
-GROUP BY clients.company HAVING SUM(billing.amount) > 3500;
+
+-- USE [databasename] to connect to specific database
+USE twitter;
+
+-- CRUD application --------------------------------------------------------------------
+-- SQL Commands - INSERT, SELECT, UPDATE, DELETE
+-- INSERT --------------------------------------
+INSERT INTO table_name1 (column_name1, column_name2)
+VALUES ('column1_value', 'column2_value');
+
+-- SELECT --------------------------------------
+SELECT categories AS name1
+FROM table1 
+WHERE conditionals -- LIKE "%", <> "%" not like, 
+ORDER BY category1 -- DESC for most RECENT at TOP
+LIMIT 3,5; -- OFFSET (ignore) first 3, starts from 4th data set
+		  -- Limit to next 5 data sets
+          
+-- UPDATE --------------------------------------
+	-- IMPORTANT: if WHERE condition is not added to the UPDATE statement, the changes will be applied to every record in the table.
+UPDATE table_name SET column_name1 = 'some_value', column_name2='another_value' 
+WHERE conditions;
+
+-- DELETE --------------------------------------
+	-- IF receiving error regarding SQL SAFE UPDATES, run the following command to bypass
+    SET SQL_SAFE_UPDATES = 0;
+	-- To proceed to DELETE records
+    DELETE FROM table_name WHERE conditions;
+	-- IMPORTANT: if WHERE condition is not added to the DELETE statement, it will delete all the records on the table
