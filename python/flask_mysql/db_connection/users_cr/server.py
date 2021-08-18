6f417337e184d584adbd7468d7049c2ededa72b8
Ryan Park <hunjinee@gmail.com>
2021-08-18 02:47:12 -0700

    Adding Assignment Files

--- python/flask_mysql/db_connection/users_cr/server.py
+++ python/flask_mysql/db_connection/users_cr/server.py
@@ -4,64 +4,31 @@ from user import User
 app = Flask(__name__)
 app.secret_key = "my_secret_key"
 
-# Home Page
-@app.route('/')
-def index():
-    return render_template("index.html")
 
-# Create user page
-@app.route('/create_user', methods=['POST'])
-def users():
-    data = {
-        "fname": request.form["fname"],
-        "lname": request.form["lname"],
-        "email": request.form["email"]
-    }
-    User.save(data)
-    user_id = User.get_by_id(data)
-    print(user_id)
-    return redirect(f'/users/{user_id}')
+@app.route('/')
+def homepage():
+    return render_template("create.html")
 
 # All Users Page
 @app.route('/users')
 def show():
     users = User.get_all()
-    return render_template("users.html", all_users = users)
+    return render_template("read_all.html", all_users = users)
 
-# User Page for Id = #
-@app.route('/users/<int:x>')
-def show_user(x):
+# Create user page
+@app.route('/users/new', methods=['POST'])
+def create_user():
     data = {
-        "id": x
-    }
-    user = User.show(data)
-    return render_template("user.html", user = user)
-
-# EDIT User Page for Id = # 
-@app.route('/users/<int:x>/edit')
-def edit(x):
-    return render_template("edit.html", x = x)
-
-# Update Form Page
-@app.route('/update', methods = ['POST'])
-def update():
-    data = {
-        "id": request.form['id'],
         "fname": request.form["fname"],
         "lname": request.form["lname"],
         "email": request.form["email"]
     }
-    edit_user = User.update(data)
-    return redirect(f"/users/{data['id']}")
+    User.save(data) # save new user data
+    return redirect('/users') # redirects to /users
 
-# DESTROY DATA for User Page for Id = #
-@app.route('/users/<int:x>/destroy')
-def destroy(x):
-    data = {
-        "id": x
-    }
-    destroy_user = User.destroy(data)
-    return redirect('/users')
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
