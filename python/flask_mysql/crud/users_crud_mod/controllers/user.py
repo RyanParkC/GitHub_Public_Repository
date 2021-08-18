2d723029ee125e2190c21be8ada765c3e6d19042
Ryan Park <hunjinee@gmail.com>
2021-08-18 03:30:52 -0700

    Adding Assignment Files

---
+++ python/flask_mysql/crud/users_crud_mod/controllers/user.py
@@ -0,0 +1,51 @@
+# Import the function that will return an instance of a connection
+from mysqlconnection import connectToMySQL
+
+# Model the class after the friend table from our database
+class User:
+    def __init__(self, data):
+        self.id = data["id"]
+        self.first_name = data["first_name"]
+        self.last_name = data["last_name"]
+        self.email = data["email"]
+        self.created_at = data["created_at"]
+        self.updated_at = data["updated_at"]
+    # We use class methods to query our database
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM users_db;" #query to select ALL columns from users table
+        results = connectToMySQL("user_db").query_db(query) 
+        #query_db returns list of dict where each dict reps a row in the table
+        users = [] #users array/list
+        for result in results:
+            users.append(cls(result))
+        return users
+
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO users_db (first_name, last_name, email, created_at, updated_at) VALUES (%(fname)s, %(lname)s, %(email)s, NOW(), NOW());"
+        return connectToMySQL("user_db").query_db(query, data)
+#--------------------------------------------------------------------------------------------------------------------
+    @classmethod
+    def get_by_id(cls, data):
+        result = []
+        query = "SELECT id FROM users_db WHERE first_name = %(fname)s AND last_name = %(lname)s"
+        result = connectToMySQL("user_db").query_db(query,data)
+        return result[0]['id']
+
+    @classmethod
+    def show(cls, data):
+        query = "SELECT * FROM users_db WHERE id = %(id)s;"
+        return connectToMySQL("user_db").query_db(query, data)
+        
+    @classmethod
+    def update(cls, data):
+        query = "UPDATE users_db SET first_name = %(fname)s, last_name = %(lname)s, email = %(email)s, updated_at = NOW() WHERE id= %(id)s;"
+        return connectToMySQL("user_db").query_db(query, data)
+
+    @classmethod
+    def destroy(cls, data):
+        query = "DELETE FROM users_db WHERE id = %(id)s"
+        return connectToMySQL("user_db").query_db(query, data)
+    def __repr__(self):
+        return f"id: {self.id}, first_name: {self.first_name}, last_name: {self.last_name}, email: {self.email}, created_at {self.created_at}, updated_at: {self.updated_at}\n"
---
+++ python/flask_mysql/crud/users_crud_mod/server.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, redirect, request
+# import the class from user.py
+from user import User
+app = Flask(__name__)
+app.secret_key = "my_secret_key"
+
+# Home Page
+@app.route('/')
+def homepage():
+    return render_template("create.html")
+
+# Create user page
+@app.route('/users/new', methods=['POST'])
+def create_user():
+    data = {
+        "fname": request.form["fname"],
+        "lname": request.form["lname"],
+        "email": request.form["email"]
+    }
+    User.save(data) # save new user data
+    user_id = User.get_by_id(data)
+    print(user_id)
+    return redirect(f'/users/{user_id}')
+
+# All Users Page
+@app.route('/users')
+def show():
+    users = User.get_all()
+    return render_template("read_all.html", all_users = users)
+
+# -----------------------------------------------------------------
+
+# User Page for Id = #
+@app.route('/users/<int:x>')
+def show_user(x):
+    data = {
+        "id": x
+    }
+    user = User.show(data)
+    return render_template("user.html", user = user[0])
+
+# EDIT User Page for Id = # 
+@app.route('/users/<int:x>/edit')
+def edit(x):
+    return render_template("edit.html", x = x)
+
+# Update Form Page
+@app.route('/update', methods = ['POST'])
+def update():
+    data = {
+        "id": request.form['id'],
+        "fname": request.form["fname"],
+        "lname": request.form["lname"],
+        "email": request.form["email"]
+    }
+    edit_user = User.update(data)
+    return redirect(f"/users/{data['id']}")
+
+# DESTROY DATA for User Page for Id = #
+@app.route('/users/<int:x>/destroy')
+def destroy(x):
+    data = {
+        "id": x
+    }
+    destroy_user = User.destroy(data)
+    return redirect('/users')
+
+if __name__ == "__main__":
+    app.run(debug=True)
