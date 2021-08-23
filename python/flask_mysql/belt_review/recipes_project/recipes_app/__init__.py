6eb1128c6ae8ed12f46d29089e4d3307ae695848
Ryan Park <hunjinee@gmail.com>
2021-08-22 22:57:33 -0700

    Updating Assignment Files

--- python/flask_mysql/belt_review/recipes_project/recipes_app/__init__.py
+++ python/flask_mysql/belt_review/recipes_project/recipes_app/__init__.py
@@ -1,4 +1,3 @@
-# __init__.py lets Python interpreter know that a directory contains code for a Python module
 from flask import Flask
 app = Flask(__name__)
 app.secret_key = 'my_secret_key'
--- python/flask_mysql/belt_review/recipes_project/server.py
+++ python/flask_mysql/belt_review/recipes_project/server.py
@@ -1,6 +1,4 @@
-# imports class 'app' from folder 'log_reg_app'
 from recipes_app import app
-# imports class 'users' from folder 'controllers' (which contains URL routes) from folder 'log_reg_app'
 from recipes_app.controllers import users_controller, recipes_controller
 
 if __name__=='__main__':
