2784e6e9da54a465031b5d9da64e42815525b49f
Ryan Park <hunjinee@gmail.com>
2021-09-08 00:23:50 -0700

    Adding Files

---
+++ MERN/react/functional_components/apps/big_inversion_app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import PersonCardComponent from './components/PersonCardComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonCardComponent firstName={"Jane"} lastName={"Doe"} age={45} hairColor={"Black"}/>
+      <PersonCardComponent firstName={"John"} lastName={"Smith"} age={88} hairColor={"Brown"}/>
+      <PersonCardComponent firstName={"Millard"} lastName={"Fillmore"} age={50} hairColor={"Brown"}/>
+      <PersonCardComponent firstName={"Maria"} lastName={"Smith"} age={62} hairColor={"Brown"}/>
+    </div>
+  );
+}
+
+export default App;
