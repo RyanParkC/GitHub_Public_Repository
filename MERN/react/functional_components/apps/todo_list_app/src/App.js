469b4c3665ea607635d78f0b33b4d69be92a3a26
Ryan Park <hunjinee@gmail.com>
2021-09-10 00:29:03 -0700

    Adding files

---
+++ MERN/react/functional_components/apps/todo_list_app/src/App.js
@@ -0,0 +1,114 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  
+  // const newTodoStateArr = useState("");
+  // const newTodo = newTodoStateArr[0];
+  // const setNewTodo = newTodoStateArr[1];
+
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleNewTodoSubmit = (event) => {
+    event.preventDefault();
+
+    // to make sure you can't add a blank text to the list
+    if (newTodo.length === 0) {
+      return;
+    }
+
+    const todoItem = {
+      text: newTodo,
+      complete: false
+    };
+    /* setTodos and pass in a brand new array containing all current todos plus 1 more */
+    setTodos([...todos, todoItem]);
+    setNewTodo("");
+  };
+
+// For Delete Button
+  const handleTodoDelete = (delIdx) => {
+    const filteredTodos = todos.filter((todo, i) => {
+      return i !== delIdx;
+    });
+
+    setTodos(filteredTodos);
+  }
+
+  const handleToggleComplete = (idx) => {
+    const updatedTodos = todos.map((todo, i) => {
+      if (idx === i) {
+        todo.complete = !todo.complete;
+        // To avoid mutating the todo directly, do this (best practice):
+        // const updatedTodo = { ...todo, complete: !todo.complete };
+        // return updatedTodo;
+      }
+
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  }
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      {
+        [<h1>hello1</h1>, <h2>world1</h2>]
+      }
+      <form 
+        onSubmit={(event) => {
+          handleNewTodoSubmit(event);
+        }}
+      >
+        <input 
+          onChange={(event) => {
+            setNewTodo(event.target.value);
+          }} 
+          type="text"
+          value={newTodo}
+        />
+        <div>
+          <button>Add</button>
+        </div>
+      </form>
+
+      <hr />
+
+      {todos.map((todo, i) => {             // displays added input on webpage
+        const todoClasses = ["bold", "italic"];
+
+        if (todo.complete) {
+          todoClasses.push("line-through");
+        }
+        return (
+          <div key={i}>
+            <input 
+              onChange={(event) => {
+                handleToggleComplete(i);
+              }} 
+              checked={todo.complete} 
+              type="checkbox" 
+            />
+            <span className={todoClasses.join(" ")}>{todo.text}</span>
+            <button 
+              onClick={(event) => {
+                handleTodoDelete(i);
+              }}
+              style={{ marginLeft: "10px" }}
+            >
+              Delete
+              </button>
+          </div>
+        );
+      })}
+
+      {
+        [<h1>hello2</h1>, <h2>world2</h2>]
+      }
+
+    </div>
+  );
+}
+
+export default App;
--- MERN/react/functional_components/notes/functional_components_notes.js
+++ MERN/react/functional_components/notes/functional_components_notes.js
@@ -19,21 +19,140 @@
 
 /* Functional Components ------------------------------------------------------------------------------- 
     E.g. Writing React Components with Functions */
-    import React from 'react';
-    const PersonCard = props => {   // here, we use an arrow function that accepts an argument "props"
-        return(
-            <div>
-                <h1>{ props.lastName }, { props.firstName }</h1>   // access values via "props.valueName"
-                <p>Age: { props.age }</p>
-                <p>Hair Color: { props.hairColor }</p>
-            </div>
-        );
-    }
-    export default PersonCard;
+        import React from 'react';
+        const PersonCard = props => {   // here, we use an arrow function that accepts an argument "props"
+            return(
+                <div>
+                    <h1>{ props.lastName }, { props.firstName }</h1>   // access values via "props.valueName"
+                    <p>Age: { props.age }</p>
+                    <p>Hair Color: { props.hairColor }</p>
+                </div>
+            );
+        }
+        export default PersonCard;
 
     // We can render the Component like this:
         <PersonCard firstName="John" lastName="Smith" age={ 8 } hairColor="Brown" /> 
             //Note: we can pass down a string with or without curly braces.
+
+/* useState ---------------------------------------------------------------------------------------------
+    Hooks = *** EXTREMELY IMPORTANT part of React ****
+    
+    Hook = A function, either built-in or custom, 
+            that allows you to "hook" in or use a certain piece of functionality.
+    
+    e.g. of "Class" Components */
+        import React, { Component } from 'react';
+            
+            
+        class Counter extends Component {
+            constructor(props) {
+                super(props);
+                this.state = {                          // we have access to "this.state"
+                    clickCount: 0
+                }
+            }
+        
+            handleClick = () => this.setState({         // and can change it via "setState"
+                clickCount: this.state.clickCount + 1
+            })
+            
+            render() {
+                return (
+                    <div>
+                        { this.state.clickCount }
+                        <button onClick={ this.handleClick }>Click Me</button>
+                    </div>
+                );
+            }
+        }
+            
+        export default Counter;
+
+/* Using useState 
+    However, in Functional component, we do NOT have such abilities.
+        Thus, we need "hooks" 
+            With "Hooks", we can define and set the "state" of a Functional Component.
+    To use useState, we must import: */
+        import React, { useState } from 'react';
+
+    /* The hook, "useState" will be imported from "react".
+        Next, let's convert that "Class" Component to a Functional Component. */
+            import React, { useState } from 'react';
+                
+                
+            const Counter = props => {
+                const [state, setState] = useState({
+                    clickCount: 0
+                });
+                
+                return(
+                    <div>{ state.clickCount }</div>
+                );
+            }
+                
+            export default Counter;
+
+/* We have just two (2) variables:
+        "state" and "setState" --> by calling the "useState" method.
+            They will behave the same way that "state" behaves in a "Class" Component.
+                We can ACCESS the current state via "state" 
+                and then CHANGE the state via "setState"
+    
+    e.g. setting "state" */
+        import React, { useState } from 'react';
+        
+        
+        const Counter = props => {                      // constant "Counter" = props (properties)
+            const [state, setState] = useState({        // constant = [state, setState] = useState
+                clickCount: 0                           // state = clickCount = 0
+            });
+        
+            const handleClick = () => {
+                setState({                              // setState = state.clickCount + 1
+                    clickCount: state.clickCount + 1    
+                });
+            }
+        
+            return (                                    // onClick "button" to click counter + 1
+                <div>
+                    { state.clickCount }
+                    <button onClick={ handleClick }>Click Me</button>  
+                </div>
+            );      // Whenever we click the button, we are preventing the default behavior and then
+        }           // seting the state by incrementing the clickCount property.
+                    // Now, we can use "state" within a Functional Component.
+        
+        export default Counter;
+    
+/* Note:
+    When invoking "useState", we do NOT need to pass in an Object.
+    We can just pass in a primitive value and then update it accordingly.
+    
+    You will more commonly see useState implemented as in the following example:
+
+    e.g. useState implemented */
+        import React, { useState } from 'react';
+        
+        
+        const Counter = props => {                      // constant "Counter" = props (properties)
+            const [count, setCount] = useState(0);      // constant [count, setCount] = useState(0)
+        
+            const handleClick = () => {                 // constant "handleClick" = () = setCount
+                setCount(count + 1);                    // setting counter + 1 for click
+            }
+        
+            return (                                    // onClick "button" to click counter + 1 
+                <div>
+                    { count }
+                    <button onClick={ handleClick }>Click Me</button>
+                </div>
+            );
+        }
+            
+        export default Counter;
+
+/* Forms Notes ------------------------------------------------------------------------------------------
 
 
 
