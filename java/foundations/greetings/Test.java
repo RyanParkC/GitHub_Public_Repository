429dff89837c40f37a6320d4c674c3475f6638b5
Ryan Park <hunjinee@gmail.com>
2021-09-25 20:11:09 -0700

    Adding files

---
+++ java/foundations/greetings/Test.java
@@ -0,0 +1,14 @@
+// Java Fundamentals
+
+// File name must be the same as the class name (must be written in PascalCase)
+    // File name = class Name
+// Every file IS a class
+public class Test {
+    // The file or class with the exact method signature below
+    // is always the launching point of the application
+    // You CANNOT run a Java program without it.
+    public static void main(String[] args) {
+        // For now, all your code goes here!
+        System.out.println("Hello world!");
+    }
+}
